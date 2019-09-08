74aff330c535d137fea321f8acd0b746343dbd0a
ashish0077 <ashish007722@gmail.com>
2019-09-08 14:06:04 +0530

    [DynamicArray.c] update display(), add()
    created two new functions display and add
    
    Signed-off-by: ashish0077 <ashish007722@gmail.com>

--- DynamicArray.c
+++ DynamicArray.c
@@ -9,7 +9,8 @@
 #include<stdbool.h>
 
 //Associating properties of dynamic array into one datatype
-struct DynamicArray {
+struct DynamicArray 
+{
     int size; //actual size of the array
     int length; //no. of elements in the array
     int* arr; //array itself
@@ -20,9 +21,16 @@ typedef struct DynamicArray darray;
 //Function protoypes
 darray* createArray(int size);
 void add(darray* arr, int element);
+void display(darray* darr);
 
-int main (void) {
+int main (void) 
+{
+    darray* arr = createArray(5);
+    for(int i = 0 ; i < 10; i++) {
+        add(arr, i);
+    }
 
+    display(arr);
     return 0;
 }
 
@@ -30,18 +38,55 @@ int main (void) {
     This function allocates memory for new array,
     and initializes the basic properties of array
 */
-darray* createArray(int size) {
+darray* createArray(int size) 
+{
     darray* newArray = (darray*) malloc(sizeof(darray)); //allocated memory for DynamicArray structure
-    newArray->arr = (int*) malloc(size*sizeof(int)); //allocating memory of size(size) for actual array which is inside the dynamic array structure 
+    newArray->arr = (int*) calloc(size, sizeof(int)); //allocating memory of size(size) for actual array which is inside the dynamic array structure
+    printf("\ncreated array at : %p\n", newArray->arr);
     newArray->length = 0; //initally length will be 0
     newArray->size = size; //initial size
+    printf("\ncreated array size and length : %d, %d\n", newArray->size, newArray->length);
 
     return newArray;
 }
 
-void add(darray* arr, int element) {
-    if(arr->length < arr->size) {
+/*
+    This function adds the element to the end of the array,
+    and if there is no space in the array it reallocates it
+*/
+void add(darray* darr, int element) 
+{
+    darr->length++; //incrementing the length
+    if (darr->length <= darr->size) 
+    {
+        //inserting the element inside the array
+        int* arr = darr->arr;
+        arr = arr + ((darr->length) - 1);
+        *arr = element;
+        printf("\n%d, inserted at location %p\n", element, arr);
+    } 
+    else 
+    {
+        printf("\narray is full, reallocating for space\n");
+        //reallocating the array
+        darr->size = (int) (darr->size + (darr->size) * 0.5);
+        darr->arr = (int*) realloc(darr->arr, (darr->size) * sizeof(int));
+        printf("\nreallocated array size : %d\n", darr->size);
+        printf("\nreallocated array address : %p\n", darr->arr);
+        //inserting the element
+        int* arr = darr->arr;
+        arr = arr + ((darr->length) - 1);
+        *arr = element;
+        printf("\n%d, inserted at location %p\n", element, arr);
+    }    
+}
 
+void display(darray* darr)
+{
+    int* arr = darr->arr;
+    for(int i = 0; i < darr->length; i++) 
+    {
+        int n = *(arr + i);
+        printf("Element %d, at address %p : %d\n", i,(arr + i), n);
     }
-    
 }
