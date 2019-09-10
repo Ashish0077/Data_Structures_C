284e81784a2d9cfc09ba3658a1b5781db592a375
ashish0077 <ashish007722@gmail.com>
2019-09-10 21:51:03 +0530

    [LinkedList.c] Initial file
    Signed-off-by: ashish0077 <ashish007722@gmail.com>

---
+++ LinkedList.c
@@ -0,0 +1,22 @@
+/*
+    Singly LinkedList implementation fro integer type data
+    
+    head -> [data][next] -> [data][next] -> [data][next] -> null
+*/
+
+//header files
+#include<stdio.h>
+#include<stdbool.h>
+#include<stdlib.h>
+
+//Structure for the Node of LinkedList
+struct node {
+    int data;
+    struct node* next;
+};
+
+//main function starts here
+int main (void) {
+
+    return 0;
+}
