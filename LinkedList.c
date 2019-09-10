47a812be73e4c84b15e6c00da7376b08e32a1893
ashish0077 <ashish007722@gmail.com>
2019-09-10 22:13:14 +0530

    [LinkedList.c] added createNode()
    This function allocates memory for newNode takes data input and initializes next to null and returns the address to the newNode
    
    Signed-off-by: ashish0077 <ashish007722@gmail.com>

--- LinkedList.c
+++ LinkedList.c
@@ -1,5 +1,5 @@
 /*
-    Singly LinkedList implementation fro integer type data
+    Singly LinkedList implementation for integer type data
     
     head -> [data][next] -> [data][next] -> [data][next] -> null
 */
@@ -14,9 +14,29 @@ struct node {
     int data;
     struct node* next;
 };
+typedef struct node NODE;
+
+//function prototypes
+NODE* createNode();
 
 //main function starts here
 int main (void) {
 
+    //Test Code
+    NODE* head = createNode();
+
     return 0;
+}
+
+/*
+    This function allocates memory for newNode
+    takes data input and initializes next to null
+    and returns the address to the newNode
+*/
+NODE* createNode () {
+    NODE* newNode = (NODE*) malloc(sizeof(NODE));
+    newNode->next = NULL;
+    printf("Enter Data : ");
+    scanf("%d", &newNode->data);
+    return newNode;
 }
