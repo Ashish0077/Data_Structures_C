ed43d5941c4d8b65da7f06ef690774f8cc4f9592
ashish0077 <ashish007722@gmail.com>
2019-09-12 05:04:36 +0530

    [LinkedList.c] insertNode(), deleteNode(), getListSize()
    Signed-off-by: ashish0077 <ashish007722@gmail.com>

--- LinkedList.c
+++ LinkedList.c
@@ -19,6 +19,9 @@ typedef struct node NODE;
 //function prototypes
 NODE* createNode();
 NODE* getNode();
+void insertNode(NODE** addressOfHead, int position);
+void deleteNode(NODE** addressOfHead, int position);
+int getListSize(NODE* head);
 
 //main function starts here
 int main (void) {
@@ -26,6 +29,9 @@ int main (void) {
     //Test Code
     NODE* head = createNode();
 
+    for(int i = 0; i < 4; i++) {
+        insertNode(&head, getListSize(head));
+    }
     return 0;
 }
 
@@ -45,7 +51,6 @@ NODE* createNode () {
 /*
     This function returns a node at the given position
 */
-
 NODE* getNode(NODE* head, int position) {
     NODE* list = head;
     int counter = 1;
@@ -58,4 +63,59 @@ NODE* getNode(NODE* head, int position) {
         counter++;
     }
     return list;
+}
+
+/*
+    This function will insert node at any given location
+*/
+void insertNode(NODE** addressOfHead, int position) {
+
+    NODE* newNode = createNode(); //create a new node
+    NODE* list;
+
+    if(position == getListSize(*addressOfHead)) {
+        list = getNode(*addressOfHead, position); //getting the node at the position
+        list->next = newNode; //making next of the fetched node equals to the new node
+    } else if(position == 1) {
+        NODE* cHead = *addressOfHead;
+        newNode->next = cHead;
+        *addressOfHead = newNode;
+    }
+    else {
+        list = getNode(*addressOfHead, position - 1); //getting the node just before the position
+        newNode->next = list->next; //setting the new node next to the the node at the position (position-1 + 1 = position)
+        list->next = newNode;
+    }
+
+}
+
+/*
+    This function will delte node at any given location
+*/
+void deleteNode (NODE** addressOfHead, int pos) {
+
+    NODE* list = *addressOfHead;
+    if (pos == 1) {
+        *addressOfHead = list->next;
+        free(list);
+    }
+    else {
+        NODE* loc = getNode(list, pos-1);
+        list = getNode(list, pos);
+        loc->next = list->next;
+        free(list);
+    }
+
+    printf("[DELETED] Node at Position : %d\n", pos);
+}
+
+int getListSize(NODE* head) {
+    NODE* list = head;
+    int counter = 1; //counting from 1
+    while(list->next != NULL) {
+        list = list->next;
+        counter++;
+    }
+
+    return counter;
 }
