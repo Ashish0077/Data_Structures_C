f8257ec1f8ceba7c8c436bfd334e59228bbd73de
Ashish0077 <ashish007722@gmail.com>
2019-09-12 08:00:21 +0000

    [DoublyLinkedList.c] initial file
    Signed-off-by: Ashish0077 <ashish007722@gmail.com>

---
+++ DoublyLinkedList.c
@@ -0,0 +1,23 @@
+/*
+    Doubly LinkedList implementation for integer type data
+
+    NULL <- [prev][data][next] <-> [prev][data][next] <-> [prev][data][next] <-> null
+*/
+
+//header files
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+//Structure for the Node of DoublyLinkedList
+struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev;
+};
+
+//main function starts here
+int main (void) {
+
+    return 0;
+}
