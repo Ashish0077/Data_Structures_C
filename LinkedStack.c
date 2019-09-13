d54a37fdd7e2c5c0949cd5c0e977cbd110d95a7c
ashish0077 <ashish007722@gmail.com>
2019-09-13 12:05:28 +0530

    [LinkedStack.c] Initial file
    Signed-off-by: ashish0077 <ashish007722@gmail.com>

---
+++ LinkedStack.c
@@ -0,0 +1,21 @@
+/*
+    *This file contains an implementation of an integer only stack using LinkedList
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+//Structure for the LinkedStack
+struct LinkedStack {
+    int data;
+    struct LinkedStack* next;
+};
+
+typedef struct LinkedStack Stack;
+
+//main function starts here
+int main(void) {
+
+    return 0;
+}
+
