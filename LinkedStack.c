efc207d46083bd07273377214b66fc8a653f41c3
ashish0077 <ashish007722@gmail.com>
2019-09-13 13:19:30 +0530

    [LinkedStack.c] push()
    Signed-off-by: ashish0077 <ashish007722@gmail.com>

--- LinkedStack.c
+++ LinkedStack.c
@@ -13,9 +13,48 @@ struct LinkedStack {
 
 typedef struct LinkedStack Stack;
 
+//Function prototype
+void push(Stack** stack, int element);
+int pop(Stack** stack);
+//void display(Stack* top);
+
 //main function starts here
 int main(void) {
+
+    //test case
+    Stack* stack = NULL;
+    for(int i = 0; i < 10; i++) {
+        push(&stack, i);
+        //display(stack);    
+    }
 
     return 0;
 }
 
+void push(Stack** stack, int element) {
+    printf("Element %d, is pushed into the stack\n", element);
+    
+    if(*stack == NULL) {
+        *stack = (Stack*) malloc(sizeof(Stack));
+        (*stack)->data = element;
+        (*stack)->next = NULL;
+    } else {
+        Stack* newElement = (Stack*) malloc(sizeof(Stack));
+        newElement->data = element;
+        newElement->next = *stack;
+        *stack = newElement;
+    }
+
+}
+
+/*
+void display(Stack* top) {
+    Stack* copy = top;
+    printf("\n\n\t \t");
+    while(copy != NULL){
+        printf("\n|\t %d \t|", copy->data);
+        copy = copy->next;
+    }
+    printf("\n\n");
+}
+*/
