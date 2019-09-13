fbf4fa54e82751330ad6e534f5ac700b0bb6082d
ashish0077 <ashish007722@gmail.com>
2019-09-13 11:01:15 +0530

    [ArrayStack.c] push() This function is used to insert elements into the stack
    Signed-off-by: ashish0077 <ashish007722@gmail.com>

--- ArrayStack.c
+++ ArrayStack.c
@@ -9,16 +9,37 @@
 //size of the stack
 #define SIZE 10
 
-struct Stack {
+struct ArrayStack {
     int top;
     int data[SIZE];
 };
 
+typedef struct ArrayStack Stack;
+
 //Function Prototypes
-void push();
+void push(Stack stack, int element);
 void pop();
 
+//main function starts here
 int main (void) {
 
+    //Test Case
+    Stack stack;
+    for(int i = 0; i < 10; i++) {
+        push(stack, i);
+    }
     return 0;
+}
+
+/*
+    This function is used to insert elements in the stack
+*/
+void push(Stack stack, int element){
+    if(stack.top == SIZE) {
+        printf("OVERFLOW, stack is full\n");
+        return;
+    }
+    stack.top++;
+    stack.data[stack.top] = element;
+    printf("Element %d is pushed into the stack\n", element);
 }
