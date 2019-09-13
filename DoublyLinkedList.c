79636f248f1ff5ad6ac09f7b51f20c6caa0f04a8
ashish0077 <ashish007722@gmail.com>
2019-09-13 09:50:22 +0530

    [DoublyLinkedList.c] insertNode(), deleteNode(), getListSize()
    Signed-off-by: ashish0077 <ashish007722@gmail.com>

--- DoublyLinkedList.c
+++ DoublyLinkedList.c
@@ -22,12 +22,29 @@ typedef struct Node NODE;
 NODE* createNode();
 NODE* getNode(NODE* head, int position);
 void printList(NODE* head);
+void insertNode(NODE** addressOfHead, int position);
+int getListSize(NODE* head);
+void deleteNode(NODE** addressOfHead, int position);
 
 //main function starts here
 int main (void) {
 
     //Test Case
     NODE* head = createNode();
+    for(int i = 0; i < 4; i++) {
+        insertNode(&head, getListSize(head));
+    }
+    printList(head);
+    printf("[INSERT] Node at position 3\n");
+    insertNode(&head, 3);
+    printList(head);
+    printf("[INSERT] Node at position 1\n");
+    insertNode(&head, 1);
+    printList(head);
+    deleteNode(&head, 2);
+    deleteNode(&head, 1);
+    printList(head);
+    printList(head);
 
     return 0;
 }
@@ -73,4 +90,68 @@ void printList(NODE* head) {
         }
     }
     printf("\n\n");
+}
+
+/*
+    This function will delete node at any given location
+*/
+void deleteNode(NODE** addressOfHead, int position) {
+    NODE* list = *addressOfHead;
+    if(position == 1){
+        *addressOfHead = list->next;
+        (*addressOfHead)->prev = NULL;
+        list->next = NULL;
+        list->prev = NULL;
+        free(list);
+    } else {
+        NODE* loc = getNode(list, position);
+        (loc->prev)->next = loc->next;
+        loc->next = NULL;
+        loc->prev = NULL;
+        free(loc);
+    }
+
+    printf("[DELETED] Node at Position : %d\n", position);
+}
+
+/*
+    This function will insert node at any given location
+*/
+void insertNode(NODE** addressOfHead, int position) {
+
+    NODE* loc;
+    NODE* newNode = createNode();
+
+    if(position == getListSize(*addressOfHead)) {
+        loc = getNode(*addressOfHead, position);
+        loc->next = newNode;
+    } else if(position ==  1) {
+        NODE* chead = *addressOfHead;
+        newNode->next = chead;
+        chead->prev = newNode;
+        *addressOfHead = newNode;
+    } else {
+        loc = getNode(*addressOfHead, position - 1);
+        NODE* loc2 = loc->next;
+        loc2->prev = newNode;
+        newNode->next = loc2;
+        loc->next = newNode;
+        newNode->prev = loc;
+    }
+}
+
+/*
+    This function will returns the number of nodes in the list
+*/
+int getListSize(NODE* head) {
+
+    NODE* list = head;
+    int counter = 1;
+
+    while(list->next != NULL) {
+        list = list->next;
+        counter++;
+    }
+
+    return counter;
 }
