18eb4407259ab3dcc5ae4328d418ffe24ee425ab
ashish0077 <ashish007722@gmail.com>
2019-09-14 11:15:11 +0530

    [ArrayQueue.c] createQueue(), enqueue(), displayQueue()
    Signed-off-by: ashish0077 <ashish007722@gmail.com>

--- ArrayQueue.c
+++ ArrayQueue.c
@@ -17,7 +17,59 @@ struct ArrayQueue {
 
 typedef struct ArrayQueue Queue;
 
+//Function prototypes
+Queue createQueue();
+void enqueue(Queue* addressOfQueue, int element);
+void displayQueue(Queue queue);
+
+//main function starts here
 int main (void) {
 
+    //test code
+    Queue queue = createQueue();
+    for(int i = 0; i < 4; i++) {
+        enqueue(&queue, i);
+    }
+
+    displayQueue(queue);
+    
     return 0;
+}
+
+/*
+    this function is used to create new Queues
+    it initializes the front and rear to -1 and then returns the newly created queue
+*/
+Queue createQueue() {
+    Queue queue = {-1, -1};
+    return queue;
+}
+
+/*
+    this function is used to insert elements in the queue
+    it increments the rear;
+*/
+void enqueue(Queue* addressOfQueue, int element) {
+    Queue queue = *addressOfQueue;
+    if(queue.front == queue.rear && queue.front == -1) {
+        queue.front = 0;
+        queue.rear = 0;
+        queue.data[queue.rear] = element;
+    } else {
+        queue.rear++;
+        queue.data[queue.rear] = element;
+    }
+
+    *addressOfQueue = queue;
+}
+
+/*
+    This function simply prints the queue
+*/
+void displayQueue(Queue queue) {
+    printf("Queue : { ");
+    for(int i = queue.front; i <= queue.rear; i++) {
+        printf("%d, ", queue.data[i]);
+    }
+    printf("\b\b }\n");
 }
