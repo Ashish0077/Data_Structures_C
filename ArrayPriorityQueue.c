4676ded133617fac7419cd638ad321d35e8ad574
ashish0077 <ashish007722@gmail.com>
2019-09-15 20:41:38 +0530

    [ArrayPriorityQueue.c] createQueue(), insert(), display()
    Signed-off-by: ashish0077 <ashish007722@gmail.com>

--- ArrayPriorityQueue.c
+++ ArrayPriorityQueue.c
@@ -5,6 +5,7 @@
 #include<stdlib.h>
 #include<stdbool.h>
 
+//size of the Queue
 #define SIZE 10
 
 //this structure represents a element of the Queue which is associated with a priority
@@ -22,8 +23,54 @@ struct ArrayPriorityQueue
     Element data[SIZE];
 };
 
+typedef struct ArrayPriorityQueue Queue;
+
+//Function prototypes
+Queue createQueue();
+void insert(Queue* addressOfQueue, int data, int priority);
+void display(Queue queue);
+
 //main function starts here
 int main (void) {
 
+    Queue queue = createQueue();
+    for(int i = 0; i < 5; i++) {
+        insert(&queue, i + 1, i);
+    }
+
+    display(queue);
+
     return 0;
+}
+
+Queue createQueue() {
+    Queue Queue;
+    Queue.front = -1;
+    Queue.rear = -1;
+
+    return Queue;
+}
+
+void insert(Queue* addressOfQueue, int data, int priority) {
+    Queue queue = (*addressOfQueue);
+    Element element;
+    element.data = data;
+    element.priority = priority;
+    if(queue.front == queue.rear && queue.front == -1) {
+        queue.front++;
+        queue.rear++;
+        queue.data[queue.rear] = element;
+        *addressOfQueue = queue;
+    } else {
+        queue.rear++;
+        queue.data[queue.rear] = element;
+        *addressOfQueue = queue;
+    }
+}
+
+void display(Queue queue) {
+    printf("DATA\tPRIORITY\n");
+    for(int i = queue.front; i <= queue.rear; i++) {
+        printf("%d\t%d\n", queue.data[i].data, queue.data[i].priority);
+    }
 }
