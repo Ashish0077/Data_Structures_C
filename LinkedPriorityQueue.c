aaf3f6ceecbac59094d4959895dd97721421dc7f
ashish0077 <ashish007722@gmail.com>
2019-09-16 14:33:26 +0530

    [LinkedPriorityQueue.c] createElement(), insert()
    Signed-off-by: ashish0077 <ashish007722@gmail.com>

--- LinkedPriorityQueue.c
+++ LinkedPriorityQueue.c
@@ -22,12 +22,17 @@ typedef struct LinkedPriorityQueue Queue;
 
 //function prototypes
 Queue createQueue();
+Element* createElement(int data, int priority);
+void insert(Queue* addressOfQueue, int data, int priority);
 
 //main function starts here
 int main (void) {
 
     //test code
     Queue queue = createQueue();
+    for(int i = 0; i < 5; i++) {
+        insert(&queue, i, i+1);
+    }
 
     return 0;
 }
@@ -40,4 +45,34 @@ Queue createQueue() {
     queue.front = NULL;
     queue.rear = NULL;
     return queue;
+}
+
+/*
+    This function creates a new Element
+    initializes data and priority
+    and returns it
+*/
+Element* createElement(int data, int priority) {
+    Element* newElement = (Element*) malloc(sizeof(Element));
+    newElement->data = data;
+    newElement->priority = priority;
+    newElement->next = NULL;
+    return newElement; 
+}
+
+/*
+    this function is used to insert elements in the Queue
+*/
+void insert(Queue* addressOfQueue, int data, int priority) {
+    Element* newElement = createElement(data, priority);
+    Queue queue = *addressOfQueue;
+    if(queue.front == NULL && queue.rear == NULL) {
+        queue.front = newElement;
+        queue.rear = newElement;
+    } else {
+        queue.rear->next = newElement;
+        queue.rear = newElement;
+    }
+
+    *addressOfQueue = queue;
 }
