32854ccf8caf69e84ae68a010cf10c0f783b1d9e
ashish0077 <ashish007722@gmail.com>
2019-09-22 15:53:11 +0530

    [SelectionSort.c] getMaxIndex(), swap(), SelectionSort()
    Signed-off-by: ashish0077 <ashish007722@gmail.com>

--- Sorting/SelectionSort.c
+++ Sorting/SelectionSort.c
@@ -4,22 +4,61 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdio.h>
+#include<time.h>
 
 //function prototypes
+void swap(int* a, int* b);
+int getMaxIndex(int arr[], int start, int end);
+void SelectionSort(int arr[], int size);
+
 
 //main function starts here
 int main (void) {
 
     //test Code
-    int arr[100];
+    int arr[10];
 
     // Use current time as seed for random generator
     srand(time(0));
 
     //initializing the array with random numbers
-    for(int i = 0; i < 100; i++) {
+    for(int i = 0; i < 10; i++) {
         arr[i] = rand();
     }
 
+    printf("Sorted Array : - \n");
+    SelectionSort(arr, 10);
+    for(int i = 0; i < 10; i++) {
+        printf("%d\n", arr[i]);
+    }
+
     return 0;
+}
+
+//This function is used to swap two values (pass by reference)
+void swap (int* a, int* b)  {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+//returns the maximum element index
+int getMaxIndex(int arr[], int start, int end) {
+    int maxIndex = start;
+    for(int i = start; i < end; i++) {
+        if(arr[maxIndex] < arr[i]) {
+            maxIndex = i;
+        }
+    }
+    return maxIndex;
+}
+
+//Selection Sort
+void SelectionSort(int arr[], int size) {
+
+    for(int i = 0; i < size; i++) {
+        int maxIndex = getMaxIndex(arr, i, size);
+        if(arr[i] < arr[maxIndex])
+            swap(&arr[i], &arr[maxIndex]);
+    }
 }
