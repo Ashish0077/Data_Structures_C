a5b7c8c46264051a12576b4caa657e2a71c8174c
ashish0077 <ashish007722@gmail.com>
2019-09-22 11:37:55 +0530

    [LinearSearch.c] basic implementation of linear search
    Signed-off-by: ashish0077 <ashish007722@gmail.com>

---
+++ Searching/LinearSearch.c
@@ -0,0 +1,31 @@
+/*
+    This File Contains very basic implementation of linear search
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+void LinearSearch(int element,int arr[], int size);
+
+int main (void) {
+    int arr[] = {0, 1, 9, 6, 2, 4, 3};
+
+    LinearSearch(6, arr, sizeof(arr)/sizeof(arr[0]));
+
+    return 0;
+}
+
+void LinearSearch(int element, int arr[], int size) {
+    
+    bool flag = false;
+    for(int i = 0; i < size; i++) {
+        if(arr[i] == element) {
+            printf("Element Found at index : %d\n", i);
+            flag = true;
+            break;
+        }
+    }
+    if(!flag) {
+        printf("Element not found\n");
+    }
+}
